232e2bea794be803ea547bccafb02e90107e6af6
Francesca <francescamariaferrero@gmail.com>
2022-03-02 12:34:35 +0100

    added name option

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Step 1: test for null.
+        if (args.Length >= 1)
+        {
+            string name = args[0];
+            Console.Write("Hello, " + name);
+        }
+        else
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
